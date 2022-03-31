f5cef0cdc56b62877c56b70d325f7d6279c32c5f
HetPatel438 <49523241+HetPatel438@users.noreply.github.com>
2022-03-31 17:35:07 +0530

    Update install_sift.sh

--- install_sift.sh
+++ install_sift.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+# Install SIFT Workstation Tools - tested to work on Ubuntu 16.04
+# j3rmbadger
+
+# Snag the binaries - https://github.com/sans-dfir/sift-cli
 wget https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-linux
-wget https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-linux.sha256.asc1
+wget https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-linux.sha256.asc
 
 # Validate signature file
 gpg --keyserver pgp.mit.edu --recv-keys 22598A94
@@ -8,5 +13,5 @@ gpg --verify sift-cli-linux.sha256.asc
 sudo mv sift-cli-linux /usr/local/bin/sift
 sudo chmod 755 /usr/local/bin/sift
 
-#Install Sift
+# Install SIFT
 sift install
